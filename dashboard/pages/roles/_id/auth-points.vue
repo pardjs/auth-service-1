1301bd9ea55c7fc47a80348e0a13db75b8ff8115
Bai Guodong <bgd_sh@hotmail.com>
2019-04-27 18:55:30 +0800

    chore(): move ui to separate project

--- dashboard/pages/roles/_id/auth-points.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <section class="section">
-    <h2 class="title is-3 has-text-grey">
-      Set Role's AuthPoints
-      <b-icon icon="rocket" size="is-large" />
-    </h2>
-  </section>
-</template>
